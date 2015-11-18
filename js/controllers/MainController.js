95aa59a0b1e018bc97dc393b1f0cf6b91751d2ce
Messier82 <seiksga@gmail.com>
2015-11-19 00:10:05 +0200

    18.11.2015 Updated Materialize CSS to version v0.97.3 and added offset fixes from theirs github. Navbar color changing: text and indicator is now white and background color is the same, as the top navbar(blue) Added showImageBar and hideImageBar function to MainController's Page item. Register form: changed labels to Materialize's animated labels; changed button's color to "blue lighten-1'; placed button in "card-action" object(this causes MaterializeCSS bug - shadow transition not working. Already posted it on their guthub) Deleted "Register" button from navbar and changed "Login" to "Sign in". Navbar title now is showing current page title if page isn't in the main menu

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -3,13 +3,35 @@
  * Design and code - M82.eu
  */
 mainModule.factory('Page', function () {
-    var title = 'Def Title';
+    var title = 'Home';
+    var siteTitle = 'Reppier';
+    var navbarTitle = 'Home';
     return {
         title: function () {
-            return title;
+            return title + " - " + siteTitle;
         },
         setTitle: function (newTitle) {
-            return title = newTitle;
+            title = newTitle;
+        },
+        navbarTitle: function () {
+            return navbarTitle;
+        },
+        setNavbarTitle: function (newTitle, isPageInMenu) {
+            $(".currentPage").removeClass("projectTitle");
+            if (isPageInMenu) {
+                navbarTitle = 'Reppier';
+                $(".currentPage").addClass("projectTitle");
+            } else {
+                navbarTitle = newTitle;
+            }
+        },
+        showImageBar: function () {
+            $("#imageBar").show();
+            $("#content").css("margin-top", "-80px");
+        },
+        hideImageBar: function () {
+            $("#imageBar").hide();
+            $("#content").css("margin-top", "20px");
         }
     };
 });
@@ -19,7 +41,7 @@ var MainController = mainModule.controller('MainController', ['$scope', 'Page',
         $scope.Page = Page;
     }]);
 
-MainController.directive("navBar", function(){
+MainController.directive("navBar", function () {
     return {
         restrict: 'E',
         templateUrl: "./views/navbar.html"
