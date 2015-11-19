51b514b112db71c13d363ccb34ebc202ada8e9b5
Messier82 <seiksga@gmail.com>
2015-11-19 21:07:37 +0200

    19.11.2015 - Login form and loading spinner Finished login form view Added loading spinner

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Reppier management system
  * Design and code - M82.eu
  */
-var mainModule = angular.module('Reppier', ['ngRoute']);
+var mainModule = angular.module('Reppier', ['ngRoute', 'angular-loading-bar', 'ngAnimate']);
 
 mainModule.config(function ($routeProvider) {
     $routeProvider
@@ -21,6 +21,11 @@ mainModule.config(function ($routeProvider) {
                 controller: "LoginController"
             });
 });
+
+mainModule.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+                cfpLoadingBarProvider.includeBar = false;
+                cfpLoadingBarProvider.spinnerTemplate = '<div class="loading-bar-spinner z-depth-2"><svg class="spinner" width="30px" height="30px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg></div>';
+            }]);
 
 $(document).ready(function () {
     $('.parallax').parallax();
